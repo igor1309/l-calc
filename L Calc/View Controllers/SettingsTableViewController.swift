dab9df3618f0b1dcad54b04e5043e117f0fa5f4c
Igor <igor1309@gmail.com>
2018-01-20 13:55:55 +0300

    use decimals switch state recorded in user defaults

--- L Calc/View Controllers/SettingsTableViewController.swift
+++ L Calc/View Controllers/SettingsTableViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
     
-    var useDecimals: Bool
+    var useDecimals = false
+    @IBOutlet weak var useDecimalsSwitch: UISwitch!
     
     @IBAction func useDecimalsSwitch(_ sender: UISwitch) {
         UserDefaults.standard.set(sender.isOn,
@@ -20,24 +21,12 @@ class SettingsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let useDecimals = UserDefaults.standard.bool(forKey: "UseDecimals")
+        useDecimalsSwitch.isOn = UserDefaults.standard.bool(forKey: "UseDecimals")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
     }
 
 }
