d80b441ebf0432ce897e07752c9c45c370f6d819
Igor <igor1309@gmail.com>
2018-02-22 21:27:11 +0300

    FIXME: collection view animation if autolayout

--- L Calc/View Controllers/CollectionViewController.swift
+++ L Calc/View Controllers/CollectionViewController.swift
@@ -63,3 +63,51 @@ extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDa
         return cell
     }
 }
+
+extension CollectionViewController: UIScrollViewDelegate {
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // Cover Image Parallax from
+        // https://designcode.io/swift4-3d-animation
+        // using just for Collection View
+        
+        if let collectionView = scrollView as? UICollectionView {
+            for cell in collectionView.visibleCells as! [SectionCollectionViewCell] {
+                let indexPath = collectionView.indexPath(for: cell)!
+                let attributes = collectionView.layoutAttributesForItem(at: indexPath)!
+                let cellFrame = collectionView.convert(attributes.frame, to: view)
+                
+                let translationX = cellFrame.origin.x / 3
+                cell.coverImageView.transform = CGAffineTransform(translationX: translationX, y: 0)
+                
+//                cell.layer.transform = animateCell(cellFrame: cellFrame)
+            }
+        }
+    }
+    
+    func animateCell(cellFrame: CGRect) -> CATransform3D {
+        // CATransform3D Rotate
+        // from https://designcode.io/swift4-3d-animation
+        
+        let angleFromX = Double((-cellFrame.origin.x) / 15)
+        let angle = CGFloat((angleFromX * Double.pi) / 180.0)
+        var transform = CATransform3DIdentity
+        transform.m34 = -1.0/1000
+        let rotation = CATransform3DRotate(transform, angle, 0, 1, 0)
+        
+        var scaleFromX = (1000 - (cellFrame.origin.x - 200)) / 1000
+        let scaleMax: CGFloat = 1.0
+        let scaleMin: CGFloat = 0.6
+        if scaleFromX > scaleMax {
+            scaleFromX = scaleMax
+        }
+        if scaleFromX < scaleMin {
+            scaleFromX = scaleMin
+        }
+        let scale = CATransform3DScale(
+            CATransform3DIdentity, scaleFromX, scaleFromX, 1)
+        
+        return CATransform3DConcat(rotation, scale)
+    }
+
+}
