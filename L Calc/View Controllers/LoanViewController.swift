1b793be9cb3086beb737f6958444dc80b4011345
Igor <igor1309@gmail.com>
2018-02-23 18:56:30 +0300

    gradient as UIView extension — no vars and funcs in Loan VC

--- L Calc/View Controllers/LoanViewController.swift
+++ L Calc/View Controllers/LoanViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 
 class LoanViewController: UIViewController {
     
-//    var gradientLayer: CAGradientLayer!
-    var colorSets = [[CGColor]]()
-    var currentColorSet: Int!
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.applyGradient()
-//        createColorSets()
-//        createGradientLayer()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -50,33 +44,4 @@ class LoanViewController: UIViewController {
     }
      */
 
-
-    func createGradientLayer() {
-        // https://www.appcoda.com/cagradientlayer/
-        
-        let gradientLayer: CAGradientLayer = CAGradientLayer()
-        gradientLayer.frame = self.view.bounds
-        gradientLayer.colors = colorSets[currentColorSet]
-        gradientLayer.locations = [0.0, 1.0]
-        gradientLayer.startPoint =
-            CGPoint(x: 188/375, y: 154/812)
-        gradientLayer.endPoint =
-            CGPoint(x: 1.0, y: 1.0)
-        
-        self.view.layer.addSublayer(gradientLayer)
-    }
-    
-    func createColorSets() {
-        colorSets.append([UIColor.red.cgColor, UIColor.yellow.cgColor])
-        colorSets.append([UIColor.green.cgColor, UIColor.magenta.cgColor])
-        colorSets.append([
-            UIColor(rgb: 0x1D2C40).cgColor,
-            UIColor(rgb: 0x364961).cgColor])
-        colorSets.append([
-            UIColor(red: 29, green: 44, blue: 64).cgColor,
-            UIColor(red: 54, green: 73, blue: 97).cgColor
-            ])
-        
-        currentColorSet = 2
-    }
 }
