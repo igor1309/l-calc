f2a63c7686e865174599f09b3d395146872cf267
Igor <igor1309@gmail.com>
2018-03-04 01:01:38 +0300

    layout not working with nav bar — would try without

--- L Calc/View Controllers/LoanMainViewController.swift
+++ L Calc/View Controllers/LoanMainViewController.swift
@@ -1,25 +1,28 @@
 //
-//  LoanViewController.swift
+//  LoanMainViewController.swift
 //  L Calc
 //
-//  Created by Igor Malyarov on 23.02.2018.
+//  Created by Igor Malyarov on 04.03.2018.
 //  Copyright © 2018 Igor Malyarov. All rights reserved.
 //
 
 import UIKit
 
-class LoanViewController: UIViewController {
+class LoanMainViewController: UIViewController {
+    
+    @IBOutlet weak var sum: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.view.applyGradient()
         
+        sum.text = "456,123"
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        
         //FIXME: TODO: нужно разобраться, как отменять эти изменения
         // чтобы navigationController в последующих VC не портить
         if let navBar = self.navigationController?.navigationBar {
@@ -30,21 +33,4 @@ class LoanViewController: UIViewController {
         }
         
     }
-    
-/*
-     override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        //FIXME: too buggy code — animation sucks
-        if let navBar = self.navigationController?.navigationBar {
-            navBar.isTranslucent = false
-
-        self.navigationController?
-            .navigationBar.setBackgroundImage(nil, for: UIBarMetrics.default)
-        self.navigationController?
-            .navigationBar.barTintColor = .black
-        }
-    }
-     */
-
 }
